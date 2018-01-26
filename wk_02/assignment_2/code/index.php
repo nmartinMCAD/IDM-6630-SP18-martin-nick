a0be02b1f92293c24683bc9d0257d409e6b56639
nmartinMCAD <nmartin@mcad.edu>
2018-01-26 10:00:53 -0700

    Styling Issues
    Sass appears to be compiling and writing to the CSS, which appears to
    be linked correctly, but I can’t seem to get things to changes.

---
+++ wk_02/assignment_2/code/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Nick's Picks</title>
+    <link href='https://fonts.googleapis.com/css?family=Exo+2|Roboto:900' rel='stylesheet'>
+    <link rel='stylesheet' href='css/css.css'>
+  </head>
+  <body>
+
+    <main>
+      <h1>Nick's Picks</h1>
+      <h2>All the Best Movies According to Yours Truly</h2>
+
+    </main>
+
+  </body>
+</html>
