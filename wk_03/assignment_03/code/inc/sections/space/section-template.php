8fc4532286efcc0605bc021fd7a8a6c5d8474086
Unknown <ralgeo@mcad.edu>
2018-02-02 19:33:56 -0600

    Section and Article Loop Refinements

--- wk_03/assignment_03/code/inc/sections/space/section-template.php
+++ wk_03/assignment_03/code/inc/sections/space/section-template.php
@@ -2,6 +2,6 @@
 
 <h2>This is the Space Section.</h2>
 
-<a href="/?article=article-01">articleOne</a>
-<a href="/?article=article-02">articleTwo</a>
-<a href="/?article=article-03">articleThree</a>
+<a href="?section=space&article=article-01">articleOne</a>
+<a href="?section=space&article=article-02">articleTwo</a>
+<a href="?section=space&article=article-03">articleThree</a>
